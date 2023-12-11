1d84946927e4d82788029f2854714cf9a4d2e51a
mridulSehgal <mridul.sehgalsnp@gmail.com>
2023-12-12 01:58:11 +0530

    1. Added _redirects in the public folder  2. Removed link of mui from loginPage footer

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
-import Link from '@mui/material/Link';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
@@ -18,9 +17,9 @@ function Copyright(props) {
   return (
     <Typography variant="body2" color="text.secondary" align="center" {...props}>
       {'Copyright © '}
-      <Link color="inherit" href="https://mui.com/">
-        AKS
-      </Link>{' '}
+
+      AKS
+
       {new Date().getFullYear()}
       {'.'}
     </Typography>
